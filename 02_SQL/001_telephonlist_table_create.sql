5227bbee184fbbc921fde4313a80706db9887546
jinny <eujin.mstory@gmail.com>
2024-05-08 18:17:34 +0900

    0508

---
+++ 02_SQL/001_telephonlist_table_create.sql
@@ -0,0 +1,46 @@
+-- 테이블명
+-- call_list : 전화번호부
+-- call_link : 전화번호 링크
+-- user_info : 회원정보
+-- call_number : 전화번호
+
+
+
+-- user_info : 회원정보
+create table if not exists user_info(
+	user_no int auto_increment primary key comment '유저번호',
+	user_name varchar(255) not null comment '유저이름',
+	user_address varchar(255) not null comment '유저주소'
+)engine=innodb comment '회원정보';
+
+-- call_list : 전화번호부
+
+create table if not exists call_list(
+	call_list_code int auto_increment comment '전화번호부코드',
+	user_no int unique comment '유저번호',
+	constraint pk_call_list_code primary key(call_list_code),
+	constraint fk_user_no foreign key(user_no) references user_info (user_no)
+)engine = innodb comment '전화번호부';
+
+-- call_number : 전화번호
+ 
+DROP TABLE call_number;
+
+create table if not exists call_number(
+ 	call_code int primary key auto_increment comment '전화번호코드',
+ 	user_no int comment '유저번호',
+ 	call_name varchar(255) not null comment '전화번호 구분',
+ 	call_number varchar(255) not null comment '전화번호',
+ 	main_call varchar(255) check(main_call in ('y', 'n')) comment '상태(대표번호)',
+	CONSTRAINT fk_ref_user_no foreign key(user_no) references user_info (user_no)
+ ) Engine = innodb comment '전화번호';
+
+-- 전화번호 링크
+
+ create table if not exists call_link(
+ 	call_list_code int comment '전화번호부 코드',
+ 	call_code int comment '전화번호 코드',
+ 	constraint fk_call_list_code foreign key (call_list_code) references user_info(user_no),
+ 	constraint fk_call_code foreign key (call_code) references user_info (user_no)
+ ) engine = innodb comment '전화번호 링크';
+
---
+++ 02_SQL/002_telephonlist_table_insert.sql
@@ -0,0 +1,9 @@
+-- 데이터 삽입
+
+select * from user_info;
+select * from call_number;
+
+
+insert into user_info values (null, '김다영', '면목동');
+
+insert into call_number values (null, null, '핸드폰', '010-8846-5954', 'Y');
