3af0d2a7f68d306b11123f9f55df7c4ae1c8fcdf
jinny <eujin.mstory@gmail.com>
2024-05-13 18:53:20 +0900

    git

--- test/001_telephonlist_table_create.sql
+++ test/001_telephonlist_table_create.sql
@@ -7,6 +7,7 @@
 
 
 -- user_info : 회원정보
+DROP TABLE user_info;
 create table if not exists user_info(
 	user_no int auto_increment primary key comment '유저번호',
 	user_name varchar(255) not null comment '유저이름',
@@ -14,11 +15,14 @@ create table if not exists user_info(
 )engine=innodb comment '회원정보';
 
 -- call_list : 전화번호부
-
+DROP TABLE call_list;
 create table if not exists call_list(
-	call_list_code int auto_increment comment '전화번호부코드',
+	call_list_code int auto_increment primary key comment '전화번호부코드',
 	user_no int unique comment '유저번호',
-	constraint pk_call_list_code primary key(call_list_code),
+	call_name varchar(255) not null comment '전화번호 구분',
+ 	call_number varchar(255) not null comment '전화번호',
+ 	main_call varchar(255) check(main_call in ('y', 'n')) comment '상태(대표번호)',
+-- 	constraint pk_call_list_code primary key(call_list_code),
 	constraint fk_user_no foreign key(user_no) references user_info (user_no)
 )engine = innodb comment '전화번호부';
 
@@ -28,7 +32,7 @@ DROP TABLE call_number;
 
 create table if not exists call_number(
  	call_code int primary key auto_increment comment '전화번호코드',
- 	user_no int comment '유저번호',
+ 	user_no int not null comment '유저번호',
  	call_name varchar(255) not null comment '전화번호 구분',
  	call_number varchar(255) not null comment '전화번호',
  	main_call varchar(255) check(main_call in ('y', 'n')) comment '상태(대표번호)',
@@ -37,10 +41,14 @@ create table if not exists call_number(
 
 -- 전화번호 링크
 
+DROP TABLE call_link;
  create table if not exists call_link(
  	call_list_code int comment '전화번호부 코드',
  	call_code int comment '전화번호 코드',
  	constraint fk_call_list_code foreign key (call_list_code) references user_info(user_no),
- 	constraint fk_call_code foreign key (call_code) references user_info (user_no)
+ 	constraint fk_call_code foreign key (call_code) references call_number (call_code)
  ) engine = innodb comment '전화번호 링크';
+
+
+
 
--- test/002_telephonlist_table_insert.sql
+++ test/002_telephonlist_table_insert.sql
@@ -2,7 +2,8 @@
 
 select * from user_info;
 select * from call_number;
-
+select * from call_list;
+-- select * from call_link;
 
 insert into user_info values (null, '김다영', '면목동');
 insert into user_info values (null, '김재석', '고잔동');
@@ -16,5 +17,28 @@ insert into call_number values (null, 3, '핸드폰', '010-8985-3027', 'Y');
 insert into call_number values (null, 4, '핸드폰', '010-6270-4016', 'Y');
 insert into call_number values (null, 5, '핸드폰', '010-2863-0788', 'Y');
 
+
+
 truncate from call_number
 where user_no = 5;
+
+select 
+	a.user_no,
+	a.user_name,
+	a.user_address,
+	b.call_name,
+	b.call_number,
+	b.main_call
+	from user_info a
+	join call_number b on a.user_no = b.user_no;
+
+SELECT
+            A.USER_NO,
+            A.USER_NAME,
+            A.USER_ADDRESS,
+            B.CALL_NAME,
+            B.CALL_NUMBER,
+            B.MAIN_CALL
+        FROM USER_INFO A
+        JOIN CALL_NUMBER B ON A.USER_NO = B.USER_NO
+        WHERE A.USER_NAME = '서은진';
